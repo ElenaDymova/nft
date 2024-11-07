7ed862fd19a95e598886a96b7f54c8bb18982d25
Elena <ebe2017@yandex.ru>
2024-11-07 10:06:39 +0300

    font and mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -28,11 +28,16 @@ popup.addEventListener('click', function(event) {
 const openMenuButton = document.querySelector('.header-button');
 const closeMenuButton = document.querySelector('.mobile-close');
 const mobileMenu = document.querySelector('.mobile');
+const mobileBg = document.querySelector('.mobile-bg');
 
 openMenuButton.addEventListener('click', () => {
     mobileMenu.classList.add('mobile-active'); 
+    mobileBg.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
 });
 
 closeMenuButton.addEventListener('click', () => {
     mobileMenu.classList.remove('mobile-active');
+    mobileBg.style.display = 'none';
+    document.body.style.overflow = '';
 });
